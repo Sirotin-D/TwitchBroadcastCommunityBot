e1d8be6fd2f5882e308e0b99d148e78e5c1b1e1e
Sirotin-D <dimsirotin151000@gmail.com>
2022-09-28 09:07:34 +0300

    Make POST and GET request functions more general

--- dev_requests.py
+++ dev_requests.py
@@ -1,35 +1,11 @@
 import requests
-import config
 
 
-def post_request():
-    url = config.auth_Twitch_URL
-    data = {"client_id": config.client_id,
-            "client_secret": config.secret_id,
-            "grant_type": config.twitch_grant_type
-            }
+def post_request(url, data) -> dict:
     response = requests.post(url=url, json=data)
-    return response
+    return response.json()
 
 
-def get_Twitch_access_token() -> str:
-    response = post_request()
-    jwt_access_token = response.json()
-    try:
-        access_token = jwt_access_token["access_token"]
-    except Exception:
-        access_token = ""
-
-    return access_token
-
-
-def get_request():
-    url = config.twitch_search_channels_url + config.twitch_channel_name
-    access_token = get_Twitch_access_token()
-    headers = {
-        "Client-ID": config.client_id,
-        "Authorization": "Bearer %s" % access_token
-    }
-
-    response = requests.get(url=url, headers=headers)
+def get_request(url, data) -> dict:
+    response = requests.get(url=url, headers=data)
     return response.json()
--- twitch_requests.py
+++ twitch_requests.py
@@ -3,8 +3,29 @@ from Broadcast import Broadcast
 import config
 
 
+def get_Twitch_access_token() -> str:
+    url = config.auth_Twitch_URL
+    data = {"client_id": config.client_id,
+            "client_secret": config.secret_id,
+            "grant_type": config.twitch_grant_type
+            }
+    response = dev_requests.post_request(url=url, data=data)
+    try:
+        access_token = response["access_token"]
+    except Exception:
+        access_token = ""
+
+    return access_token
+
+
 def is_broadcast_live() -> Broadcast:
-    response = dev_requests.get_request()
+    url = config.twitch_search_channels_url + config.twitch_channel_name
+    access_token = get_Twitch_access_token()
+    headers = {
+        "Client-ID": config.client_id,
+        "Authorization": "Bearer %s" % access_token
+    }
+    response = dev_requests.get_request(url=url, data=headers)
     channel_list = response["data"]
     streamer = dict()
     for channel in channel_list:
