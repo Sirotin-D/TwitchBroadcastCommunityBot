7ff63c2b328ec539cb8eec6fa121cc154c6e9005
Sirotin-D <dimsirotin151000@gmail.com>
2022-09-28 10:00:30 +0300

    Add default values in dev_requests funcs

--- dev_requests.py
+++ dev_requests.py
@@ -1,11 +1,11 @@
 import requests
 
 
-def post_request(url, body) -> dict:
+def post_request(url, body="") -> dict:
     response = requests.post(url=url, params=body)
     return response.json()
 
 
-def get_request(url, body) -> dict:
+def get_request(url, body="") -> dict:
     response = requests.get(url=url, headers=body)
     return response.json()
