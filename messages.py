74735b650e94fcdd3de9ba329241dc1cc7be2ce5
Sirotin-D <dimsirotin151000@gmail.com>
2022-10-18 16:48:03 +0300

    Update spacing between words in messages.py

--- messages.py
+++ messages.py
@@ -1,18 +1,18 @@
 from Config import private_config
 from DataClasses.Broadcast import Broadcast
 
-all_commands_message = "Список команд бота:\n " \
+all_commands_message = "Список команд бота:\n" \
                        "1) Стрим - узнать, идёт ли сейчас стрим\n" \
                        "2) График - узнать график стримов\n" \
                        "3) Команды - список всех команд"
 greeting_message = f"Здарова! {all_commands_message}"
-stream_schedule = "График стримов:\n " \
+stream_schedule = "График стримов:\n" \
                   "Каждый день в 19:00\n" \
                   "Суббота - выходной"
-stream_status_message = "Статус стрима: \n"
+stream_status_message = "Статус стрима:\n"
 stream_now_offline_message = "Стрим сейчас оффлайн"
 stream_now_online_message = "Стрим сейчас онлайн!\n" \
-                            "Скорее залетай на трансляцию\n " \
+                            "Скорее залетай на трансляцию\n" \
                             f"{private_config.twitch_channel_url}"
 
 
