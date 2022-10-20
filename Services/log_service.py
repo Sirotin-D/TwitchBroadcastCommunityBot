aa61999ac6dacec60a807b9e4fd793b9968d00d6
Sirotin-D <dimsirotin151000@gmail.com>
2022-10-20 11:27:06 +0300

    Add logging in file

--- Services/log_service.py
+++ Services/log_service.py
@@ -12,3 +12,9 @@ class LogService:
                       f"[{log_type.get_type()}]: " \
                       f"{message}"
         print(log_message)
+        if log_type != LogType.DEBUG:
+            log_message = f"{datetime.now():%d.%m.%Y %H:%M:%S} " \
+                          f"[{log_type.get_type()}]: " \
+                          f"{message}"
+            with open("logs.txt", "a") as log_file:
+                log_file.write(f"{log_message}\n")
